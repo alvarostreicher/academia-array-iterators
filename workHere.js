2a7fde0866a936a88f42a54dcc8f62eb36c37781
Alvaro Olvera <alvarostreicher@gmail.com>
2019-05-15 12:11:57 -0700

    Excersises Es6
    Signed-off-by: Alvaro Olvera <alvarostreicher@gmail.com>

--- workHere.js
+++ workHere.js
@@ -4,7 +4,7 @@ var billionaires = require('./billionaires.json');
     Get the names of the billionaires that are in their 20s
 */
 
-let twentyYearOldBillionaires = null;
+let twentyYearOldBillionaires = billionaires.filter((x)=>x.demographics.age >= 20 && x.demographics.age < 30);
 
 /*
     create a new object with the billionairse year, name and worth rounded to the newarest integer
@@ -15,32 +15,52 @@ let twentyYearOldBillionaires = null;
     }
 */
 
-let roundedWorth = null;
-
+let roundedWorth = billionaires.map((x)=>{
+    return {
+            year: x.year,
+            name: x.name,
+            worth: Math.round(x["wealth"]["worth in billions"])
+            }
+});
 /*
     sum the rounded worth of all the billionaires
 */
 
-let aggregatedWorth = null;
+let aggregatedWorth = roundedWorth.reduce((x, y)=> { return x + y.worth },0);
 // console.log(aggregatedWorth)
 
 /*
     find the the richest billionaire, make sure the result is in an array
 */
 
-let richest = null
-
+let richest = billionaires.reduce((prev, current) => {
+    if(Object.keys(prev).length === 0){
+     return current;
+    }else if(current.rank < prev.rank){
+        prev = current;
+    }
+    return prev;
+},{})
+richest=[richest];
 /*
     find the poorest billioanire, make sure the result is in an array
 */
 
-let poorest = null;
+let poorest = billionaires.reduce((prev, current) => {
+    if(Object.keys(prev).length === 0){
+     return current;
+    }else if(current.rank >= prev.rank){
+        prev = current;
+    }
+    return prev;
+},{})
+poorest=[poorest];;
 
 /*
     make a new array containing the richest and the poorest billionaire
 */
 
-let richAndPoor = null;
+let richAndPoor = [].concat(richest, poorest);
 
 module.exports = {
     twentyYearOldBillionaires,
